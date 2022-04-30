59451a0c6697f10984f13bd2059f0b4b1516edaa
Mike Doyle <mikedoyle007@gmail.com>
2022-04-29 23:02:51 -0700

    Add colorscheme gruvbox-community

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,8 +62,8 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   -- use "folke/tokyonight.nvim"
-  -- use "gruvbox-community/gruvbox"
-  use {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+  use "gruvbox-community/gruvbox"
+  -- use {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
   use "shaunsingh/nord.nvim"
 
   -- Completion
